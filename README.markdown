230df4fe5ce150e6f1a505899444a0aa9936b23a
Thomas Duerr <thomduerr@gmail.com>
2009-06-12 22:20:51 +0200

    simple template and readme

--- README.markdown
+++ README.markdown
@@ -5,3 +5,19 @@ templates for my rails needs
 ## usage:
 
     rails project -m simple.template.rb
+or
+
+    railst project simple
+
+For this put the following function (based on [Ryan Bates][1]) into your bashrc:
+
+    function railst {
+      appname=$1
+      template=$2
+      shift 2
+      rails $appname -m http://github.com/thomd/rails-templates/raw/master/$template.template.rb $@
+    }
+
+
+
+[1]: http://github.com/ryanb/rails-templates/tree/master
--- simple.temlate.rb
+++ simple.temlate.rb
@@ -1,1 +1,24 @@
-run "echo TODO > README"
+
+generate :nifty_layout
+generate :nifty_scaffold, "post name:string content:text index new edit"
+route "map.root :controller => 'post'"
+
+
+git :init
+
+file ".gitignore", <<-END
+.DS_Store
+log/*.log
+tmp/**/*
+config/database.yml
+db/*.sqlite3
+END
+
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run "cp config/database.yml config/database.example.yml"
+run "rm public/index.html"
+  
+git :add => ".", :commit => "-m 'initial commit'"
+
+
+rake "db:migrate"
